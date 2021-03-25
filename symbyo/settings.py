9bc4d3a6714d0bede971bd029e891db9955dc986
annalenadavis <annalenakdavis@gmail.com>
2021-03-24 20:06:04 -0400

    Use environment variable for secret key and add gitignore file

--- symbyo/settings.py
+++ symbyo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5&p!7ib6)vvju1z=_!*!rk&kru=kv3ys!9j@+bd)2l=fl*0o18'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
