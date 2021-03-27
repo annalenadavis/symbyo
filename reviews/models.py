e93952791b5487ab4c558306d76f0001becb19e0
annalenadavis <annalenakdavis@gmail.com>
2021-03-27 14:56:56 -0400

    Update model fields so not all are required and naming objects

--- reviews/models.py
+++ reviews/models.py
@@ -13,28 +13,29 @@ class Discipline(models.Model):
         return self.area
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    name = str(first_name) + str(last_name)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     email = models.EmailField()
-    portrait = models.ImageField()
+    portrait = models.ImageField(blank=True)
 
     def __str__(self):
-        return self.name
+        return self.first_name + " " + self.last_name
 
 class Trainer(Person):
-    description = models.TextField()
-    website = models.URLField()
-    disciplines = models.ManyToManyField('Discipline')
+    description = models.TextField(blank=True)
+    website = models.URLField(blank=True)
+    business = models.CharField(max_length=100, blank=True)
+    disciplines = models.ManyToManyField('Discipline', blank=True)
 
-    def __str__(self):
-        return self.name
+    if (not business):
+        def __str__(self):
+            return self.first_name + " " + self.last_name
+    else:
+        def __str__(self):
+            return self.first_name + " " + self.last_name + ", " + self.business
 
 class Student(Person):
-    teachers = models.ForeignKey('Trainer', on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.name
+    teachers = models.ForeignKey('Trainer', on_delete=models.PROTECT, blank=True)
 
 class Review(models.Model):
     trainer = models.ForeignKey('Trainer', on_delete=models.CASCADE)
