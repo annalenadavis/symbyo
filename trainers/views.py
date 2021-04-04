f1d9745a396a18d653a5f893b599a2d8b3d55d8f
annalenadavis <annalenakdavis@gmail.com>
2021-04-04 14:34:13 -0400

    Add templates for index and trainer views

--- trainers/views.py
+++ trainers/views.py
@@ -1,11 +1,21 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Trainer
 
 # See all Trainers
 def index(request):
-    return HttpResponse("Hello, world. You're at the trainers index.")
+    trainers_list = Trainer.objects.all()
+    template = loader.get_template('reviews/index.html')
+    context = {
+        'trainers_list': trainers_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 # Trainer Profile
 def trainer(request, trainer_id):
-    return HttpResponse("You're looking at trainer %s." % trainer_id)
+    template = loader.get_template('reviews/trainer.html')
+    context = {
+        'trainer_id' : trainer_id,
+    }
+    return HttpResponse(template.render(context, request))
