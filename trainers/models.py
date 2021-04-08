6365f1cafef8ad96390376e3ae7c136cef10c48d
annalenadavis <annalenakdavis@gmail.com>
2021-04-08 16:17:25 -0400

    Update models by extending user model with a one-to-one link

--- trainers/models.py
+++ trainers/models.py
@@ -1,42 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Discipline(models.Model):
-    AREA = [
-        ('MEDITATION', 'Meditation'),
-        ('MUAYTHAI', 'Muay Thai'),
-        ('MMA', 'Mixed Martial Arts'),
-        ('YOGA', 'Yoga'),       
-    ]
-    area = models.CharField(max_length=50, choices=AREA)
-
+    area = models.CharField(max_length=50)
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
+    
     def __str__(self):
         return self.area
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    portrait = models.ImageField(blank=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    teachers = models.ForeignKey('Trainer', on_delete=models.PROTECT, blank=True)
 
-    def __str__(self):
-        return self.first_name + " " + self.last_name
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-class Trainer(Person):
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Trainer(Profile):
     description = models.TextField(blank=True)
     website = models.URLField(blank=True)
     business = models.CharField(max_length=100, blank=True)
     disciplines = models.ManyToManyField('Discipline', blank=True)
 
-    if (not business):
-        def __str__(self):
-            return self.first_name + " " + self.last_name
-    else:
-        def __str__(self):
-            return self.first_name + " " + self.last_name + ", " + self.business
-
-class Student(Person):
-    teachers = models.ForeignKey('Trainer', on_delete=models.PROTECT, blank=True)
-
 class Review(models.Model):
-    trainer = models.ForeignKey('Trainer', on_delete=models.CASCADE)
-    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    # trainer = models.ForeignKey('Trainer', on_delete=models.CASCADE)
+    reviewer = models.ForeignKey('Profile', on_delete=models.CASCADE)
