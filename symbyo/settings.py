e983e00514bc843f9350ba68a43cd5aca5a41a20
annalenadavis <annalenakdavis@gmail.com>
2021-04-13 11:32:48 -0400

    Replace Profile model with Trainer model which links to user model
    Also fix issues with migrations. Fresh start.

--- symbyo/settings.py
+++ symbyo/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = '5&p!7ib6)vvju1z=_!*!rk&kru=kv3ys!9j@+bd)2l=fl*0o18'
+SECRET_KEY = os.environ.get('SECRET_KEY', '5&p!7ib6)vvju1z=_!*!rk&kru=kv3ys!9j@+bd)2l=fl*0o18')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- trainers/models.py
+++ trainers/models.py
@@ -5,30 +5,32 @@ from django.dispatch import receiver
 
 class Discipline(models.Model):
     area = models.CharField(max_length=50)
-    parent = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
+    parent = models.ForeignKey('Discipline', on_delete=models.PROTECT, null=True, blank=True)
     
     def __str__(self):
         return self.area
 
-class Profile(models.Model):
+class Trainer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    teachers = models.ForeignKey('Trainer', on_delete=models.PROTECT, blank=True)
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
-class Trainer(Profile):
     description = models.TextField(blank=True)
     website = models.URLField(blank=True)
     business = models.CharField(max_length=100, blank=True)
     disciplines = models.ManyToManyField('Discipline', blank=True)
 
+    # def clean(self):
+    #     # Code here - runs before parent clean
+    #     return_value = super().clean()
+    #     # Code here - then run this after running parent
+    #     return return_value
+
+@receiver(post_save, sender=User)
+def create_user_trainer(sender, instance, created, **kwargs):
+    if created:
+        Trainer.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_trainer(sender, instance, **kwargs):
+    instance.trainer.save()
+
 class Review(models.Model):
     trainer = models.ForeignKey('Trainer', on_delete=models.CASCADE, null=True)
-    # reviewer = models.ForeignKey('Profile', on_delete=models.CASCADE)
